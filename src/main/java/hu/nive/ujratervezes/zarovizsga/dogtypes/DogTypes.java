61305141c78d29302fb0955afc9ee3af3dd48a81
EgydGIT <egyd.eszter@gmail.com>
2021-03-16 22:00:20 +0100

    DogTypes

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/DogTypes.java
@@ -0,0 +1,42 @@
+package hu.nive.ujratervezes.zarovizsga.dogtypes;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DogTypes {
+
+    private DataSource dataSource;
+
+    public DogTypes(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public List<String> getDogsByCountry(String hungary) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement stmt =
+                        conn.prepareStatement("select name from dog_types where country = ?");
+        ) {
+            stmt.setString(1, hungary);
+            return getStrings(stmt);
+        } catch (SQLException sqle) {
+            throw new IllegalArgumentException("Error by insert", sqle);
+        }
+    }
+
+    private List<String> getStrings(PreparedStatement stmt) throws SQLException {
+        List<String> results = new ArrayList<>();
+        try (ResultSet rs = stmt.executeQuery()){
+            while (rs.next()) {
+                results.add(rs.getString("name").toLowerCase());
+            }
+            return results;
+        }
+    }
+
+}
